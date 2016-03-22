e9700c6c7ed2b615cf050a383ba5f7b40eaf4faa
jcsantos23 <jcsantos23@gmail.com>
2016-03-22 16:16:12 -0700

    Added Welcome.php view

---
+++ application/views/welcome.php
@@ -0,0 +1,6 @@
+<div class="facet">
+    <h2>Schedule</h2>
+    <p><a href="/welcome/schedule/day">Day</a></p>
+    <p><a href="/welcome/schedule/period">Period</a></p>
+    <p><a href="/welcome/schedule/course">Course</a></p>
+</div>
